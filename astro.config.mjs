013415f1d9552a2e9aef0dc1b06a5a0520d9420c
jonnjonnjo <zhenagajo234@gmail.com>
2026-05-29 23:20:00 +0700

    rework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://blog.jonarthurito.tech',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      themes: { light: 'github-light', dark: 'github-dark' },
+      wrap: true,
+    },
+  },
 });
