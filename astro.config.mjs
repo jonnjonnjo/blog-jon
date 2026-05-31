036e1234bdb5d344af644eb432e8fc890e1221ad
jonnjonnjo <zhenagajo234@gmail.com>
2026-05-31 11:26:25 +0700

    feat: anchor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 export default defineConfig({
   site: 'https://blog.jonarthurito.tech',
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+    ],
     shikiConfig: {
       themes: { light: 'github-light', dark: 'github-dark' },
       wrap: true,
